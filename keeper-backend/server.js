3422ea1de80b5a2afac7097c5130ddeecfe83379
Amar Arora <amararora456@gmail.com>
2026-02-02 00:03:55 +0530

    Fix 404 on refresh with vercel.json

--- keeper-backend/server.js
+++ keeper-backend/server.js
@@ -18,7 +18,10 @@ const allowedOrigins = [
 ];
 
 app.use(cors({ origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
